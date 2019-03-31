2698d8dbf6bd09ff67ac7b439daf56d9dca8a0b0
Doan Phu <doanphua4@gmail.com>
2019-03-31 22:49:44 +0700

    Hoàn thành chức năng quốc gia: liệt kê danh sách, thêm, sửa, xóa quốc gia

--- movie-project/routes/web.php
+++ movie-project/routes/web.php
@@ -15,4 +15,16 @@ Route::group(['prefix'=>'admin'],function(){
     Route::get('/index',function () {
         return view('admin.layout');
     });
+
+    Route::group(['prefix'=>'country'],function(){
+        Route::get('/list','Admin\CountryController@index')->name('admin_country_list');
+
+        Route::get('/create','Admin\CountryController@create')->name('admin_country_create');
+        Route::post('/store','Admin\CountryController@store')->name('admin_country_store');
+
+        Route::get('/edit/{id}','Admin\CountryController@edit')->name('admin_country_edit');
+        Route::post('/update/{id}','Admin\CountryController@update')->name('admin_country_update');
+
+        Route::post('/delete/{id}','Admin\CountryController@destroy')->name('admin_country_delete');
+    });
 });
