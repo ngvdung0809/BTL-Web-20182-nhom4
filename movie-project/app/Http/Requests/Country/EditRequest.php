1bcd1798a8e1f5d6a2c10b276d20af3907dc7f9b
Doan Phu <doanphua4@gmail.com>
2019-04-03 00:34:54 +0700

    Sửa để đồng bộ

--- movie-project/app/Http/Requests/Country/EditRequest.php
+++ movie-project/app/Http/Requests/Country/EditRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Country;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class TypeRequest extends FormRequest
+class EditRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,22 +24,21 @@ class TypeRequest extends FormRequest
     public function rules()
     {
         return [
-            //
-            'name' => 'required|min:2|max:255|unique:type,name',
-            'abbrev' => 'required|min:3|max:255',
+            'name' => ['required', 'string', 'min:3', 'max:255'],
+            'abbrev' => ['required', 'string', 'min:2', 'max:255']
         ];
     }
+
     public function  messages(){
         return [
-            'name.required'=>'Bạn chưa nhập tên thể loại',
+            'name.required'=>'Bạn chưa nhập tên quốc gia',
             'name.string'=>'Tên quốc gia là chuỗi kí tự',
-            'name.min'=>'Tên thể loại nhỏ nhất 2 kí tự',
-            'name.max'=>'Tên thể loại lớn nhất 255 kí tự',
-            'name.unique'=>'Tên thể loại đã tồn tại',
-            'abbrev.required'=>'Bạn chưa nhập tên viết tắt',
+            'name.min'=>'Tên quốc gia nhỏ nhất 3 kí tự',
+            'name.max'=>'Tên quốc gia lớn nhất 255 kí tự',
+            'abbrev.required'=>'Bạn chưa nhập tên viết tắt quốc gia',
             'abbrev.string'=>'Tên viết tắt là chuỗi kí tự',
             'abbrev.min'=>'Tên viết tắt ít nhất 2 kí tự',
-            'abbrev.max'=>'Tên viết tắt ít nhất 255 kí tự'
+            'abbrev.max'=>'Tên viết tắt ít nhất 255 kí tự',
         ];
     }
 }
