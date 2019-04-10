321a804bdc277029eed911423bec48869776bbad
Doan Phu <doanphua4@gmail.com>
2019-04-10 22:46:41 +0700

    Hoàn thành chức năng thêm, sửa, xóa và xem thông tin của phim

--- movie-project/app/Models/Film.php
+++ movie-project/app/Models/Film.php
@@ -22,4 +22,29 @@ class Film extends Model
         'share',
         'other_description'
     ];
+
+    public function actor()
+    {
+        return $this->beLongsToMany('App\Models\Person', 'film_cast', 'film_id', 'cast_id');
+    }
+
+    public function type()
+    {
+        return $this->beLongsToMany('App\Models\Type', 'film_type', 'film_id', 'type_id');
+    }
+
+    public function country()
+    {
+        return $this->beLongsTo('App\Models\Country', 'country_id');
+    }
+
+    public function publisher()
+    {
+        return $this->beLongsTo('App\Models\Publisher', 'publisher_id');
+    }
+
+     public function director()
+    {
+        return $this->beLongsTo('App\Models\Person', 'director_id');
+    }
 }
