e66460682d6afd937efb7ebeaaf87acf7ec1e3d2
Doan Phu <doanphua4@gmail.com>
2019-05-09 17:59:16 +0700

    Sửa lại route layout home về ban đầu

--- movie-project/app/Http/Controllers/Home/HomeController.php
+++ movie-project/app/Http/Controllers/Home/HomeController.php
@@ -16,11 +16,4 @@ class HomeController extends Controller
         $countryHome = Country::all();
         View::share(['typeHome'=>$typeHome, 'countryHome'=>$countryHome]);
     }
-
-    public function showHome()
-    {
-        $types = Type::all();
-        $countries = Country::all();
-        return view('home.layout', ['types'=>$types, 'countries'=>$countries]);
-    }
 }
--- movie-project/routes/web.php
+++ movie-project/routes/web.php
@@ -128,7 +128,9 @@ Route::group(['prefix'=>'admin'],function(){
 });
 
 Route::group(['prefix'=>'home'],function(){
-    Route::get('/index','Home\HomeController@showHome')->name('home_index');
+    Route::get('/index',function () {
+        return view('home.layout');
+    })->name('home_index');
 
     Route::group(['prefix'=>'/user/{user_id}'],function(){
         Route::group(['prefix'=>'/profile'],function(){
