6ec2bbd1d753a9f542f01032ec470a7a6283c3b5
ngvdung0809 <ngvdung0809@gmail.com>
2019-05-12 10:28:19 +0700

    phan quyen user

---
+++ movie-project/app/Http/Controllers/Home/LoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class LoginController extends HomeController
+{
+    //
+    public function check(Request $request){ 
+        $user=[
+            'username'=>$request->username,
+            'password'=>$request->password,
+            'role'=>'0',
+
+        ];
+
+        $admin=[
+            'username'=>$request->username,
+            'password'=>$request->password,
+            'role'=>'1'
+
+        ];
+
+
+        if(Auth::attempt($user)){
+            return redirect()->route('home_index')->with('success', 'Đăng nhập thành công');
+        }
+        elseif (Auth::attempt($admin)) {
+            # code...
+            return redirect()->route('admin_index')->with('success', 'Đăng nhập thành công');
+        }
+        else{ 
+            return redirect()->back()->with('error', 'Thông tin tài khoản hoặc mật khẩu không chính xác');
+        }
+
+    }
+}
